c3509312aaaedfba1e60eaf0808161ebd9497c49
RODRIGO ALFREDO DEL CARMEN HERNANDEZ <radel220499@gmail.com>
2025-07-20 15:10:41 -0600

    inicio de sesion

--- aplicacionWeb/src/app/app.component.ts
+++ aplicacionWeb/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./compartidos/nav-bar/nav-bar.component";
 import { FoodComponent } from "./compartidos/food/food.component";
+import { LoginComponent } from "./estaticos/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FoodComponent],
+  imports: [RouterOutlet, NavBarComponent, FoodComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ aplicacionWeb/src/app/estaticos/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  logoPath: string = '/imagenes/branding/logo.png'; 
+}
