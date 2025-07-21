2d65024f196108370e1eccc9e080ef8b7d0d2e82
RODRIGO ALFREDO DEL CARMEN HERNANDEZ <radel220499@gmail.com>
2025-07-20 18:49:38 -0600

    Creacion del componente registro

--- aplicacionWeb/src/app/app.component.ts
+++ aplicacionWeb/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./compartidos/nav-bar/nav-bar.component";
 import { FoodComponent } from "./compartidos/food/food.component";
 import { LoginComponent } from "./estaticos/login/login.component";
+import {  RegistroComponent } from "./estaticos/registro/registro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FoodComponent, LoginComponent],
+  imports: [RouterOutlet, NavBarComponent, FoodComponent, LoginComponent,RegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ aplicacionWeb/src/app/estaticos/registro/registro.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  imports: [ReactiveFormsModule],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
+})
+export class RegistroComponent {
+
+   form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group(
+      {
+        nombre: ['', Validators.required],
+        apellidoPaterno: ['', Validators.required],
+        apellidoMaterno: ['', Validators.required],
+        fechaNacimiento: ['', Validators.required],
+        especialidad: ['', Validators.required],
+        cedulaProfesional: ['', Validators.required],
+        telefono: ['', Validators.required],
+        direccion: ['', Validators.required],
+        contrasena: ['', Validators.required],
+        confirmarContrasena: ['', Validators.required],
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(formGroup: AbstractControl): ValidationErrors | null {
+    const pass = formGroup.get('contrasena')?.value;
+    const confirm = formGroup.get('confirmarContrasena')?.value;
+    return pass === confirm ? null : { passwordMismatch: true };
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      // Aquí puedes enviar los datos al backend
+    }
+}
+}
