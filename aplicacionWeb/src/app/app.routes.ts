7ee2ebfbe353dc052f29813f963073c0ebe47ef6
RODRIGO ALFREDO DEL CARMEN HERNANDEZ <55514243+Rodrigo-Alfredo-DCH@users.noreply.github.com>
2025-08-16 10:26:57 -0600

    pagina estatica
    ando trabajando el en backend y solo que a donde sali no tengo mucha señal  y perdi mi cel :(

--- aplicacionWeb/src/app/app.routes.ts
+++ aplicacionWeb/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './estaticos/inicio/inicio.component';
 import { LoginComponent } from './estaticos/login/login.component';
 import { RegistroComponent } from './estaticos/registro/registro.component';
+import { DueloPerdidaComponent } from './estaticos/duelo-perdida/duelo-perdida.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
-  { path: 'Duelo-y-perdida', component: InicioComponent },
+  { path: 'Duelo-y-perdida', component: DueloPerdidaComponent },
   { path: 'Contactos-de-apoyo', component: InicioComponent },
   { path: 'Iniciar-sesion', component: LoginComponent },
   { path: 'Registro', component: RegistroComponent },
