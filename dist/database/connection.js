0118e6b267d8d2f402e66c9c40988205e1b99eb4
Amsmonroe <108552317+Amsmonroe@users.noreply.github.com>
2025-09-30 21:48:03 -0600

    Update connection.js
    Cambiamos localhost por ip VM

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelize = new sequelize_1.Sequelize('miduelo', 'root', 'root', {
-    host: 'localhost',
+    host: '52.188.186.87',
     dialect: "mysql"
 });
 exports.default = sequelize;
