07c630773758dcb58e64de42ed045078313ab88e
Alejandra Michelle Mateo Garcia <amateog1700@alumno.ipn.mx>
2025-09-30 23:38:37 -0600

    Update database connection credentials and configuration options

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -1,11 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('miduelo', 'root', 'root', {
+const sequelize = new sequelize_1.Sequelize('miduelo', 'Michelle', 'hD*F9jBw@U6dS6Ym', {
     host: '52.188.186.87',
-    dialect: "mysql"
+    dialect: "mysql",
+    port: 3306,
+    dialectOptions: {
+        connectTimeout: 60000
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 60000,
+        idle: 10000
+    }
 });
 exports.default = sequelize;
+
+
+
+
 //const sequelize = new Sequelize('myduelo','dev_user', 'P4ssw0rd-S3gur0!',{
 //    host: 'localhost',
 //    dialect: "mysql"
