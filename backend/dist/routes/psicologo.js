a864fb2731a70f2ea5b2cc266fa31f823cae1553
Alejandra Michelle Mateo Garcia <amateog1700@alumno.ipn.mx>
2025-10-02 01:54:03 -0600

    FRONY
    f

--- backend/dist/routes/psicologo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const psicologo_1 = require("../controllers/psicologo");
-const router = (0, express_1.Router)();
-router.post("/api/psicologo/registro", psicologo_1.registro);
-router.post("/api/psicologo/iniciar-sesion", psicologo_1.login);
-exports.default = router;
--- backend/src/index.ts
+++
@@ -1,5 +0,0 @@
-import Server from "./models/server";
-import dotenv from 'dotenv';
-
-dotenv .config();
-const server = new Server();
